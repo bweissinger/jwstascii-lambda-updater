f304203523491058410f88e987672c3290ffd912
Bryan Weissinger <weissingerb@gmail.com>
2022-10-02 05:22:48 -0500

    Add jwst website image credit parsing.

--- layer/jwstascii_helpers/jwst_site_scraper.py
+++ layer/jwstascii_helpers/jwst_site_scraper.py
@@ -1,5 +1,6 @@
 import re
 from bs4 import BeautifulSoup, SoupStrainer
+from typing import List
 
 
 class Scraper:
@@ -34,3 +35,41 @@ class Scraper:
 
         strainer = SoupStrainer(["a", "p"])
         return str(BeautifulSoup(image_description, "lxml", parse_only=strainer))
+
+    def get_image_credits(self, html: str) -> List[str]:
+        """
+        Parses image credits from the footer of a jwst website image page.
+
+        Args:
+            html (str): Html of jwst website image page.
+
+        Raises:
+            ValueError: Raises value error if no footer is found, if no image prefix
+                is found, or if the resulting credits list is blank.
+
+        Returns:
+            List[str]: A list of the image credits parsed from the page.
+        """
+        soup = BeautifulSoup(html, "lxml")
+        footer = soup.find("footer")
+
+        if not footer:
+            raise ValueError("Could not find footer in html: \n%s" % html)
+
+        credits = footer.find("p", string=re.compile("IMAGE:*", re.I))
+
+        if not credits:
+            raise ValueError(
+                "Could not find credits image prefix in footer: \n%s" % html
+            )
+
+        credits_list = []
+        for credit in credits.text.split(":")[1].split(","):
+            credit = credit.replace(" ", "")
+            if credit:
+                credits_list.append(credit)
+
+        if not credits_list:
+            raise ValueError("No credits parsed from html: \n%s" % html)
+
+        return credits_list
--- tests/unit/jwst_site_scraper_test.py
+++ tests/unit/jwst_site_scraper_test.py
@@ -28,3 +28,48 @@ class TestGetImageDescription(TestCase):
     def test_no_footer(self):
         html = """<h4>About This Image</h4><p>Some text<a href="some_url" target="_self">href_text</a> continued description.</p>"""
         self.assertRaises(ValueError, self.scraper.get_image_description, html)
+
+
+class TestGetImageCredits(TestCase):
+    def setUp(self) -> None:
+        self.scraper = Scraper()
+        return super().setUp()
+
+    def test_empty_string(self):
+        self.assertRaises(ValueError, self.scraper.get_image_credits, "")
+
+    def test_no_footer(self):
+        html = """<strong>Credits:</strong><p>IMAGE: NASA, ESA, CSA, STScI</p>"""
+        self.assertRaises(ValueError, self.scraper.get_image_credits, html)
+
+    def test_no_image_prefix(self):
+        html = (
+            """<footer><strong>Credits:</strong><p>NASA, ESA, CSA, STScI</p></footer>"""
+        )
+        self.assertRaises(ValueError, self.scraper.get_image_credits, html)
+
+    def test_blank_credits(self):
+        html = """<footer><strong>Credits:</strong><p>IMAGE: </p></footer>"""
+        self.assertRaises(ValueError, self.scraper.get_image_credits, html)
+
+    def test_valid_credits(self):
+        html = """<footer><strong>Credits:</strong><p>IMAGE: NASA, ESA, CSA, STScI</p></footer>"""
+        self.assertEqual(
+            self.scraper.get_image_credits(html), ["NASA", "ESA", "CSA", "STScI"]
+        )
+
+    def test_lowercase_image_prefix(self):
+        html = """<footer><strong>Credits:</strong><p>image: NASA, ESA, CSA, STScI</p></footer>"""
+        self.assertEqual(
+            self.scraper.get_image_credits(html), ["NASA", "ESA", "CSA", "STScI"]
+        )
+
+    def test_single_credit(self):
+        html = """<footer><strong>Credits:</strong><p>image: NASA</p></footer>"""
+        self.assertEqual(self.scraper.get_image_credits(html), ["NASA"])
+
+    def test_only_parses_credit_in_footer(self):
+        html = """<p>image: NOT, CORRECT, CREDITS</p><footer><strong>Credits:</strong><p>image: NASA, ESA, CSA, STScI</p></footer>"""
+        self.assertEqual(
+            self.scraper.get_image_credits(html), ["NASA", "ESA", "CSA", "STScI"]
+        )
